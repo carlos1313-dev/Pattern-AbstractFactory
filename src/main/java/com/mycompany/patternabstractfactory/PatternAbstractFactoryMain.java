c3cfd3c9a2f4a128ed338bcca792f16554d14793
Carlos E <sangroniscarlosenrique1@gmail.com>
2026-02-17 00:17:45 -0500

    Creacion entidades

---
+++ src/main/java/com/mycompany/patternabstractfactory/PatternAbstractFactoryMain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.patternabstractfactory;
+
+/**
+ *
+ * @author sangr
+ */
+public class PatternAbstractFactoryMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
